4764ce99f4d40679ea712c5050960e629c583817
Phagie1 <aphagie@gmail.com>
2021-09-25 17:59:59 +0800

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,128 @@
+@extends('base')
+
+@section('content')
+
+<style>
+</style>
+
+  <div class="container">
+  <h1 class="title text-light" >My Computer Sets  </h1>
+  </div>
+
+  <style>
+
+    .center {
+      padding: 70px 0;
+      border: 3px solid green;
+      text-align: center;
+      margin-top: 15%;
+    }
+    </style>
+    <br>
+    <br>
+    <div class="container">
+    
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="card bg-black" style="width: 18rem;">
+              <img src="1.jpg" class="card-img-top" alt="...">
+              <div class="card-body bg-black text-light ">
+                <h5 class="card-title">ASUS VivoBook E203</h5>
+                <p class="card-text"><p>Processor	Intel Celeron N335</p>
+                                      <p>Graphics	Intel HD 500</p>
+                                      <p>Storage	32GB (expandable via microSD)</p>
+                                      <p>Memory	2GB or 4GB</p></p>
+                                      <p>Price  $189.00</p>
+                <a href="#" class="btn btn-dark">Add to cart</a>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card bg-black" style="width: 18rem;">
+              <img src="2.jpg" class="card-img-top" alt="...">
+              <div class="card-body bg-black text-light ">
+                <h5 class="card-title ">acer aspire 5</h5>
+                <p class="card-text">
+                  <p>15.6-inch 16:9 display, 1920 x 1080</p>
+                  <p>Intel Core i5-1035G1</p>
+                  <p>8GB DDR4 memory</p>
+                  <p>256GB PCIe NVMe SSD</p>
+                  <p>price $67,900</p>
+                </p>
+                <a href="#" class="btn btn-dark">Add to cart</a>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card bg-black" style="width: 18rem;">
+              <img src="3.jpg" class="card-img-top" alt="...">
+              <div class="card-body bg-black text-light ">
+                <h5 class="card-title">Toshiba Dynabook s6</h5>
+                <p class="card-text">
+                  <p>CPU: Intel Core i5-8250U processor</p>
+                  <p>Clock frequency: 1.60GHz</p>
+                  <P>Memory: 8GB</P>
+                  <P>Price P60,851</P>
+                </p>
+                <a href="#" class="btn btn-dark">Add to cart</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="card bg-black" style="width: 18rem;">
+            <img src="4.jpg" class="card-img-top" alt="...">
+            <div class="card-body bg-black text-light ">
+              <h5 class="card-title">Samsung 14 galaxy Book</h5>
+              <p class="card-text">
+                <p>CPU: Qualcomm Snapdragon 7c Gen 2.</p>
+                <p>GPU: Qualcomm Adreno GPU.</p>
+                <p>RAM: 4GB</p>
+                <p>Storage: 128GB.</p>
+                <p>Price P50,198 </p>
+              </p>
+              <a href="#" class="btn btn-dark">Add to cart</a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card bg-black" style="width: 18rem;">
+            <img src="5.jpg" class="card-img-top" alt="...">
+            <div class="card-body bg-black text-light ">
+              <h5 class="card-title">Dell XPS 13</h5>
+              <p class="card-text">
+                <p>Processor. Processor. Intel Core i5 6th Gen 6200U</p>
+                <p>Memory. RAM. 8GB</p>
+                <p> Intel Integrated HD Graphics 520</p>
+                <p>Price P144,375</p>
+              </p>
+              <a href="#" class="btn btn-dark">Add to cart</a>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card bg-black" style="width: 18rem;">
+          <div class="card " style="width: 18rem;">
+            <img src="6.jpg" class="card-img-top" alt="...">
+            <div class="card-body bg-black text-light ">
+              <h5 class="card-title">MacBook Pro 2021</h5>
+              <p class="card-text">"
+                <p>13-inch</p>
+                <p>8GB RAM</p>
+                <p>256GB SSD Storage</p>
+                <p>Price P71,990</p>
+              </p>
+              <a href="#" class="btn btn-dark">Add to cart</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+@endsection
+
